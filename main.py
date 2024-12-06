51d05d47d535eb5bd4b335f7fea17e74a99f7499
MateuszGurgul11 <134134081+MateuszGurgul11@users.noreply.github.com>
2024-12-06 11:40:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 from calculate_seats import calculate_seats_interface
 
 def main():
+    st.set_page_config(layout="wide")
     calculate_seats_interface()
 
 if __name__ == "__main__":
